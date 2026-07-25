af865293e9cc945a67cfb1fd44d28ff55b729f08
herdianaauliyanikita-code <herdianaauliyanikita@gmail.com>
2026-07-25 09:14:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mengambil tombol berdasarkan ID
+const tombol = document.getElementById("btnMulai");
+
+// Menambahkan aksi ketika tombol diklik
+tombol.addEventListener("click", function () {
+    alert("Selamat datang di EduTask! Semoga belajar semakin mudah 😊");
+});
